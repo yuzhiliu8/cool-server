ed0bd6a13415423f94bca2515daaf245cb045845
yuzhiliu8 <yuzhiliu8@gmail.com>
2025-01-30 00:45:09 -0500

    init spring boot backend

---
+++ server/src/main/java/com/coolserver/server/ServerApplication.java
@@ -0,0 +1,14 @@
+package com.coolserver.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
